7076327294816d09d3a79dbe72998598d29b29dd
Alessandro Duterte <alexduterte@gmail.com>
2020-01-31 23:50:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,6 +6,7 @@ end
 def summon_captain_planet(planeteer_calls)# code an argument here
   array = []
   array << planeteer_calls.map { |call| "#{call}!" }.capitalize
+  array
 end
 
 def long_planeteer_calls# code an argument here
